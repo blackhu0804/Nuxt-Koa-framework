311d107ef5d39a85b01edf8a46cef66ad2fa9700
blackhu <812510003@qq.com>
2018-12-04 23:48:09 +0800

    shit eslint

---
+++ layouts/test.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="layout-search">
+        <nuxt/>
+        <footer>我是测试footer</footer>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+
+<style>
+    .layout-search footer {color: red;}
+</style>
---
+++ pages/test.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="content">test</div>
+</template>
+
+<script>
+export default {
+  layout: "test"
+};
+</script>
+
+<style scoped>
+</style>
